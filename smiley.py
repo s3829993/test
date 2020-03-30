8c71492e73e15b0ba8354725db00f5c513c89a26
Ellinor <s3829993@student.rmit.edu.au>
2020-03-30 11:08:56 +1100

    added function colour

--- smiley.py
+++ smiley.py
@@ -5,3 +5,9 @@ class Smiley:
     
   def setPattern(self, pattern):
     self.pattern = pattern
+
+  def setColour(self, new_colour):
+    B = [0, 0, 0]
+    for n, i in enumerate(self.pattern):
+      if i != B:
+        self.pattern[n] = new_colour  
